35f12b525c42647c02ad4b186da3acfda69557fa
Debashish Sarkar Durjoy <dsdjoy4@gmail.com>
2021-10-05 04:33:05 +1100

    trying to find the keys

--- sdes.py
+++ sdes.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 IP = [2, 6, 3, 1, 4, 8, 5 , 7]
 IPinverse = [4, 1, 3, 5, 7, 2, 8, 6]
 P10 = [3, 5, 2, 7, 4, 10, 1, 9, 8, 6]
@@ -28,7 +30,7 @@ def decimalToBinaryRecurse(n, binArray):
 
 #this is the helper function for the recurse function
 #takes decimal base number as input
-#returns an list containing the equivalent binary number
+#returns a list containing the equivalent binary number
 def decimalToBinary(n):
     binArray = [] #declaring the list here to pass it to the recurse function
     decimalToBinaryRecurse(n, binArray) #call the actual conversion function
@@ -49,8 +51,60 @@ def applyP(charList, permutationName):
         result.append(charList[index - 1])
     return result
 
+def leftShift(arr):
+    item = arr.pop(0)
+    arr.append(item)
+    return arr
+
 # print(stringToList("A B C D E F G H I J"))
 # print(applyP(stringToList("A B C D E F G H I J"), "IP"))
-binary = decimalToBinary(15)
+binary = [1, 1, 0, 0, 1, 1, 0, 0]
+print("Original T: ")
 for i in range(len(binary)):
-    print(binary[i], end=" ")
+    print(f'{binary[i]}', end=" ")
+
+print()
+
+binary = applyP(binary, "IP")
+print("IP on Text: ")
+for i in range(len(binary)):
+    print(f'{binary[i]}', end=" ")
+
+print()
+
+rightEP = applyP(binary[-4:], "E_P")
+print("RightEP: ")
+for i in range(len(rightEP)):
+    print(f'{rightEP[i]}', end=" ")
+
+print()
+print()
+print()
+
+inputKey = [1,1,1,0,0, 1,1,0,0,0]
+temp = applyP(inputKey, "P10")
+print("on P10: ")
+for i in range(len(temp)):
+    print(f'{temp[i]}', end=" ")
+print()
+temp = leftShift(temp)
+print("on LS: ")
+for i in range(len(temp)):
+    print(f'{temp[i]}', end=" ")
+print()
+temp = applyP(temp, "P8")
+print("on P8: ")
+for i in range(len(temp)):
+    print(f'{temp[i]}', end=" ")
+
+print()
+K1 = applyP(leftShift(applyP(inputKey, "P10")), "P8")
+print("K1: ")
+for i in range(len(K1)):
+    print(f'{K1[i]}', end=" ")
+
+print()
+K2 = applyP(leftShift(leftShift(applyP(inputKey, "P10"))), "P8")
+print("K2: ")
+for i in range(len(K2)):
+    print(f'{K2[i]}', end=" ")
